1b157e99e8c80ae8d905393e17d108a76d97b768
Avadhoot1234 <avadhoot.dhumal9@gmail.com>
2025-03-04 17:37:42 +0530

    Done with the creation of header

---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Image from 'next/image'
+import { CiSearch } from "react-icons/ci";
+import { CiBellOn } from "react-icons/ci";
+import { CiChat1 } from "react-icons/ci";
+
+const Header = () => {
+  return (
+    <div className='flex gap-3 items-center p-6'>
+      <Image src='/logo.png' alt='logo' width={50} height={50} 
+      className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/>
+
+      <button className='bg-black text-white p-2 px-4 rounded-full'>Home</button>
+      <button className='font-semibold p-2 px-4 rounded-full'>Create</button>
+
+      <div className='bg-[#e9e9e9] p-3 flex gap-3 items-center rounded-full w-full'>
+        <CiSearch className='text-[25px] text-gray-500'/>
+        <input type='text' placeholder='Search' className='bg-transparent outline-none'/>
+      </div>
+
+
+      <CiBellOn className='text-[40px] text-gray-500'/>
+      <CiChat1 className='text-[40px] text-gray-500'/>
+
+      <Image src="/man.png" alt='user-image' width={50} height={50}
+      className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/>
+    </div>
+  )
+}
+
+export default Header
